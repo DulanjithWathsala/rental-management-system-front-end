a6a4c9083ee6e6eadecd7b3ae44ae176d6c9e757
M.D.W.De Silva <wdulanjith@gmail.com>
2024-07-26 17:32:02 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { ItemsComponent } from './pages/items/items.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './pages/header/header.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/rental/rental.component.ts
+++ src/app/pages/rental/rental.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rental',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './rental.component.html',
   styleUrl: './rental.component.css'
 })
 export class RentalComponent {
 
+  public itemIds = {}
+  public customerIds = {}
+
+  constructor(private http: HttpClient) {
+    this.loadIds();
+  }
+
+
+  loadIds() {
+    this.http.get("http://localhost:8080/hardware-item/retrieve-all-ids").subscribe(res => {
+      this.itemIds = res;
+    })
+
+    this.http.get("http://localhost:8080/customer/retrieve-all-ids").subscribe(res => {
+      this.customerIds = res;
+    })
+  }
 }
